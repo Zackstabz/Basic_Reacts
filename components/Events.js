65ba5b4ec9a65e5413e9b237c44b905e3c77d72e
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2024-09-19 15:09:13 -0300

    Add files via upload

---
+++ components/Events.js
@@ -0,0 +1,11 @@
+const Events =() => {
+    return(
+        <div>
+            <div>
+                <button>Clique Aqui</button>
+            </div>
+        </div>
+    )
+}
+
+export default Events;
---
+++ components/TemplateExpressions.js
@@ -0,0 +1,17 @@
+const TemplateExpressions = () => {
+  const name = "Zackstabz";
+  const data = {
+    age: 27,
+    job: "Programmer",
+  };
+
+  return (
+    <div>
+      <h1>Olá {name}, tudo bem?</h1>
+      <p>Você atua como: {data.job}</p>
+      <p> {4 + 4} </p>
+    </div>
+  );
+};
+
+export default TemplateExpressions;
